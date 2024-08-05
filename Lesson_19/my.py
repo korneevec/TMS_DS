a9d541d3b8f302f2e3dc934149e3adbe04c3d904
olegkorneevec <olegkorneevec@gmail.com>
2024-08-05 18:17:15 +0300

    init

--- Lesson_19/my.py
+++ Lesson_19/my.py
@@ -162,21 +162,21 @@ class GradientBoosting:
 
 
 if __name__ == "__main__":
-    from sklearn.datasets import load_iris
+    from sklearn.datasets import load_breast_cancer
     from sklearn.model_selection import train_test_split
     from sklearn.metrics import accuracy_score
 
-    data = load_iris()
+    data = load_breast_cancer()
     X, y = data.data, data.target
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
 
-    # clf = DecisionTree(max_depth=3, min_samples_split=2)
-    # clf.fit(X_train, y_train)
+    clf = DecisionTree(max_depth=3, min_samples_split=2)
+    clf.fit(X_train, y_train)
 
-    # y_pred = clf.predict(X_test)
-    # print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
+    y_pred = clf.predict(X_test)
+    print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
 
     gb_model = GradientBoosting()
     gb_model.fit(X_train, y_train)
